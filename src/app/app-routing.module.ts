6fb534774ada5fb0b7b88392979c366027366c99
Khalil200a <jemghili200a@gmail.com>
2022-10-26 17:44:28 +0100

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {SigninComponent} from "./pages/signin/signin.component";
+import {SignupComponent} from "./pages/signup/signup.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
   {path: '', redirectTo:'/home', pathMatch: "full"}
 ];
 
