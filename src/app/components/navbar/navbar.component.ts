118a6790fc70d5021dff84574e2b7504f03cdb9f
Khalil200a <jemghili200a@gmail.com>
2022-10-26 17:48:02 +0100

    updating navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  stickyClass: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.pageYOffset > 68) {
+      this.stickyClass = true;
+    } else {
+      this.stickyClass = false;
+    }
   }
 
 }
