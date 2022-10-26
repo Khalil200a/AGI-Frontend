bcb3875f3ebb3541d41539728b6c1a024c8ee2b0
Khalil200a <jemghili200a@gmail.com>
2022-10-26 17:44:10 +0100

    add prime ng icons and Angular Materiels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import {MatIconModule} from "@angular/material/icon";
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -9,6 +11,7 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
 import {RouterOutlet} from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,13 +20,16 @@ import { AppRoutingModule } from './app-routing.module';
     NavbarComponent,
     SigninComponent,
     SignupComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     SlickCarouselModule,
     RouterOutlet,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
