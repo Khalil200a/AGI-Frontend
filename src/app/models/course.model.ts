60077e12f6111a4fee7d11b64e8fdd9ce68658a2
Khalil200a <jemghili200a@gmail.com>
2022-10-27 12:31:18 +0100

    adding courses model

---
+++ src/app/models/course.model.ts
@@ -0,0 +1,10 @@
+export class Course {
+  course_id: number;
+  course_name: string;
+  course_desc: string;
+  course_author: string;
+  course_img: string;
+  course_duration: string;
+  course_price: string;
+  course_original_price: string;
+}
