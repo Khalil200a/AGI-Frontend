838531f43c520d283255478ded3add55d5ce23a7
Khalil200a <jemghili200a@gmail.com>
2022-11-02 22:03:00 +0100

    add component course show

---
+++ src/app/pages/course/courses-show/courses-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-show',
+  templateUrl: './courses-show.component.html',
+  styleUrls: ['./courses-show.component.scss']
+})
+export class CoursesShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
