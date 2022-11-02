599f1706eaa892514b75c0d324103a345bf1e4b9
Khalil200a <jemghili200a@gmail.com>
2022-11-02 22:04:42 +0100

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {SigninComponent} from "./pages/signin/signin.component";
 import {SignupComponent} from "./pages/signup/signup.component";
+import {CoursesIndexComponent} from "./pages/course/courses-index/courses-index.component";
+import { AuthGuard } from "./helpers/auth.guard";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'courses',
+    children:[
+      {path:'index', component: CoursesIndexComponent, canActivate: [AuthGuard]}
+    ]
+  },
   {path: '', redirectTo:'/home', pathMatch: "full"}
 ];
 
