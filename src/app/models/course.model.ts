6ec33731cbb92a4128d6ff42b98ceb4417ae2946
Khalil200a <jemghili200a@gmail.com>
2022-11-04 01:30:27 +0100

    update course model and service

--- src/app/models/course.model.ts
+++ src/app/models/course.model.ts
@@ -2,9 +2,9 @@ export class Course {
   course_id: number;
   course_name: string;
   course_desc: string;
-  course_author: string;
   course_img: string;
   course_duration: string;
   course_price: string;
   course_original_price: string;
+  courses_authors: Array<String>;
 }
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map} from "rxjs";
 import {Course} from "../models/course.model";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,33 @@ export class CourseService {
 
   coursesJson = 'assets/json/course.json';
 
+  //Get all the Courses
   getCourses() {
-    return this.http.get<Course[]>(this.coursesJson).pipe(map((res) => {
+    return this.http.get<Course[]>(`${environment.apiUrl}/courses`).pipe(map((res) => {
       return res;
     }));
+  }
+
+  //Get Course by Id
+  getCourse(id: number){
+    return this.http.get<Course>(`${environment.apiUrl}/courses/${id}`).pipe(map((res) =>{
+      return res;
+    }));
+  }
+
+  getCoursesByInstructor(){
+    return this.http.get<Course[]>(`${environment.apiUrl}/courses/instructor`).pipe(map((res)=>{
+      return res;
+    }));
+  }
+
+  getCoursesByStudent(){
+    return this.http.get<Course[]>(`${environment.apiUrl}/courses/student`).pipe(map((res)=>{
+      return res;
+    }));
+  }
+
+  addCourseToStudent(course: Course){
+    return this.http.post(`${environment.apiUrl}/courses/student/${course.course_id}`, course)
   }
 }
