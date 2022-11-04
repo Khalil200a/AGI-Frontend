8b38d14641eee715edbf283c7ffabd3a6a665026
Khalil200a <jemghili200a@gmail.com>
2022-11-04 01:28:48 +0100

    update course show

--- src/app/pages/course/courses-show/courses-show.component.ts
+++ src/app/pages/course/courses-show/courses-show.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from "../../../models/course.model";
+import {CourseService} from "../../../services/course.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-courses-show',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesShowComponent implements OnInit {
 
-  constructor() { }
+  course: Course = {} as Course;
+
+  id: number;
+
+  constructor(private courseService: CourseService,private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.courseService.getCourse(this.route.snapshot.params['id']).subscribe((res: Course)=>{
+      this.course = res;
+    })
   }
+
+  onBuy(course: Course){
+    return this.courseService.addCourseToStudent(course).subscribe((res) =>{
+      this.router.navigate(['home']);
+    });
+  }
+
+
 
 }
