d7a1f1b1a8630f933786f562ffa849e149628f62
Khalil200a <jemghili200a@gmail.com>
2022-11-04 01:30:49 +0100

    update signin & signup

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  signinForm: FormGroup;
+  constructor(
+    public fb: FormBuilder,
+    public authService: AuthService,
+    public router: Router
+  ) {
+    this.signinForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
   }
-
+  ngOnInit() {}
+  loginUser() {
+    this.authService.signIn(this.signinForm.value);
+    this.router.navigateByUrl("/home");
+  }
 }
