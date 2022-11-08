306e0a0731d2e004158d6990e4766c852b3d86e6
Khalil200a <jemghili200a@gmail.com>
2022-11-08 15:36:01 +0100

    add student-show component

---
+++ src/app/pages/student/student-show/student-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-show',
+  templateUrl: './student-show.component.html',
+  styleUrls: ['./student-show.component.scss']
+})
+export class StudentShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
