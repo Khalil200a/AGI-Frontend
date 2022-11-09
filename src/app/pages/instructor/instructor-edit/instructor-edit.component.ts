848b227c5f213a216cca69ff47d613867a3a6bf2
Khalil200a <jemghili200a@gmail.com>
2022-11-09 16:47:57 +0100

    add form and link it with course

--- src/app/pages/instructor/instructor-edit/instructor-edit.component.ts
+++ src/app/pages/instructor/instructor-edit/instructor-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {CourseService} from "../../../services/course.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Course} from "../../../models/course.model";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-instructor-edit',
@@ -11,30 +12,54 @@ import {Course} from "../../../models/course.model";
 })
 export class InstructorEditComponent implements OnInit {
 
-  courseFrom: FormGroup;
+  courseFrom = new FormGroup({
+    course_id: new FormControl(0),
+    course_name: new FormControl(''),
+    course_desc: new FormControl(''),
+    course_img: new FormControl(''),
+    course_duration: new FormControl(''),
+    course_price: new FormControl(''),
+    course_original_price: new FormControl(''),
+    courses_authors: new FormControl('')
+  });
   course: Course = {} as Course;
   id: number;
-  constructor(private _formBuilder: FormBuilder, private courseService: CourseService, private route: ActivatedRoute) { }
+  constructor(private _formBuilder: FormBuilder, private courseService: CourseService, private route: ActivatedRoute, private router: Router, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.courseService.getCourse(this.id).subscribe((res:Course) =>{
       this.course = res;
-    })
+      this.initCourseForm();
+    });
   }
 
   initCourseForm(){
-    this.courseFrom = this._formBuilder.group({
-      course_id: [{value: this.course.course_id}],
-      course_name: [{value: this.course.course_name}],
-      course_desc: this.course.course_desc,
-      course_img: this.course.course_img,
-      course_duration: this.course.course_duration,
-      course_price: this.course.course_price,
-      course_original_price: this.course.course_original_price,
-      courses_author: this.course.courses_authors[0]
-    })
-  }
+    this.courseFrom.patchValue({
+        course_id: this.course.course_id,
+        course_name: this.course.course_name,
+        course_desc: this.course.course_desc,
+        course_img: this.course.course_img,
+        course_duration: this.course.course_duration,
+        course_price: this.course.course_price,
+        course_original_price: this.course.course_original_price,
+      }
+    )
 
-  editCourse(){}
+ }
+
+ editCourse(){
+   this.course.course_name = this.courseFrom.controls['course_name'].value;
+   this.course.course_desc = this.courseFrom.controls['course_desc'].value;
+   this.course.course_duration = this.courseFrom.controls['course_duration'].value;
+   this.course.course_price = this.courseFrom.controls['course_price'].value;
+   this.course.course_original_price = this.courseFrom.controls['course_original_price'].value;
+   //TODO
+   this.courseService.updateCourse(this.course, this.course.course_id).subscribe((response) =>{
+       this.messageService.add({severity:'success', summary:'Course', detail: 'Course Updated successfully'});
+       this.router.navigate(['instructor']);
+   }
+   );
+
+ }
 }
