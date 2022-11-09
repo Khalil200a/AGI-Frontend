ad6d6567fcb57d1b245c5a84c0c37e26fb72bedd
Khalil200a <jemghili200a@gmail.com>
2022-11-09 16:44:18 +0100

    add role to sign up

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {User} from "../../models/user.model";
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  signupForm: FormGroup;
-  constructor(
-    public fb: FormBuilder,
-    public authService: AuthService,
-    public router: Router
-  ) {
-    this.signupForm = this.fb.group({
-      username: [''],
-      email: [''],
-      password: [''],
-    });
+  user = new User();
+  roles: Array<String>;
+  selected: String;
+  signupForm = new FormGroup({
+    'username' : new FormControl(''),
+    'email' : new FormControl(''),
+    'password' : new FormControl(''),
+    'role' : new FormControl(''),
+  })
+  constructor(public authService: AuthService, public router: Router) {}
+  ngOnInit() {
+    this.roles = ['ROLE_ADMIN', 'ROLE_INSTRUCTOR', 'ROLE_STUDENT'];
+    this.selected = 'ROLE_STUDENT';
   }
-  ngOnInit() {}
   registerUser() {
-    this.authService.signUp(this.signupForm.value).subscribe((res) => {
+    this.user.username = this.signupForm.controls['username'].value;
+    this.user.password = this.signupForm.controls['password'].value;
+    this.user.email = this.signupForm.controls['email'].value;
+    this.user.role = [this.signupForm.controls['role'].value];
+    this.authService.signUp(this.user).subscribe((res) => {
       if (res.result) {
         this.signupForm.reset();
-        this.router.navigate(['/signin']);
+        this.router.navigate(['signin']);
       }
     });
   }
