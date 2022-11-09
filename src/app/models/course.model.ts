1acc05f694a9ded24f7f72ab278eceeb5346c401
Khalil200a <jemghili200a@gmail.com>
2022-11-09 16:41:10 +0100

    add lessons to course model

--- src/app/models/course.model.ts
+++ src/app/models/course.model.ts
@@ -1,3 +1,5 @@
+import {Lesson} from "./lesson.model";
+
 export class Course {
   course_id: number;
   course_name: string;
@@ -6,5 +8,6 @@ export class Course {
   course_duration: string;
   course_price: string;
   course_original_price: string;
-  courses_authors: Array<String>;
+  course_authors: Array<String>;
+  course_lessons: Array<Lesson>;
 }
