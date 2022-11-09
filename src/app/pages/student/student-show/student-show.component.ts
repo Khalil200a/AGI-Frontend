7776b23530af0f8404ebd77874b28563ddf3787b
Khalil200a <jemghili200a@gmail.com>
2022-11-09 16:45:20 +0100

    add lesson to course that is viewed by student

--- src/app/pages/student/student-show/student-show.component.ts
+++ src/app/pages/student/student-show/student-show.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from "../../../models/course.model";
+import {CourseService} from "../../../services/course.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MessageService} from "primeng/api";
+import {LessonService} from "../../../services/lesson.service";
 
 @Component({
   selector: 'app-student-show',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-show.component.scss']
 })
 export class StudentShowComponent implements OnInit {
+  course: Course = {} as Course;
 
-  constructor() { }
+  id: number;
 
-  ngOnInit(): void {
+  constructor(private courseService: CourseService, private router: Router, private messageService: MessageService, private route: ActivatedRoute, private lessonService: LessonService) {
   }
 
+  ngOnInit(): void {
+    this.courseService.getCourse(this.route.snapshot.params['id']).subscribe((res: Course) => {
+      this.course = res;
+    })
+  }
+
+  onUpdate(course: Course) {
+    this.router.navigate(['instructor', 'edit', course.course_id]);
+  }
+
+  onClickEdit(lesson_id: number) {
+    this.router.navigate(['instructor', 'lesson', 'edit', lesson_id])
+  }
+
+  onClickIndex() {
+    this.router.navigate(['instructor', 'lesson', this.course.course_id])
+  }
+
+  onClickDetail(lesson_id: number) {
+    this.router.navigate(['instructor', 'lesson', 'detail', lesson_id])
+  }
 }
