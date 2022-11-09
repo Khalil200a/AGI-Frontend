5bbb2309238d593b2da5df510f7981da212a7bb3
Khalil200a <jemghili200a@gmail.com>
2022-11-09 16:46:18 +0100

    get all student and instructors for admin

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {map} from "rxjs";
+import {User} from "../models/user.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllStudents(){
+    return this.http.get<User[]>(`${environment.apiUrl}/admin/students`).pipe(map((res)=>{
+      return res;
+    }))
+  }
+
+  getAllInstructors(){
+    return this.http.get<User[]>(`${environment.apiUrl}/admin/instructors`).pipe(map((res)=>{
+      return res;
+    }))
+  }
 }
