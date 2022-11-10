dc818b56191add3a18066962972b2b289e628587
Khalil200a <jemghili200a@gmail.com>
2022-11-11 00:17:20 +0100

    fixing students by admin

--- src/app/pages/admin/student/admin-student-index/admin-student-index.component.ts
+++ src/app/pages/admin/student/admin-student-index/admin-student-index.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Course} from "../../../../models/course.model";
 import {CourseService} from "../../../../services/course.service";
 import {AuthService} from "../../../../services/auth.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {User} from "../../../../models/user.model";
 import {UserService} from "../../../../services/user.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-admin-student-index',
@@ -12,19 +13,26 @@ import {UserService} from "../../../../services/user.service";
   styleUrls: ['./admin-student-index.component.scss']
 })
 export class AdminStudentIndexComponent implements OnInit {
-  students : User[];
+  students: User[];
   admin: String;
 
-  constructor(private userService: UserService, private authService: AuthService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private userService: UserService, private messageService: MessageService, private authService: AuthService, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.admin = localStorage.getItem('username');
-    this.userService.getAllStudents().subscribe((res:User[]) =>{
+    this.userService.getAllStudents().subscribe((res: User[]) => {
       this.students = res;
     });
   }
-  //
-  // onClickDetail(id){
-  //   this.router.navigateByUrl('courses/'+id)
-  // }
+
+  onClickDetail(id: number) {
+    this.router.navigateByUrl('profile/' + id);
+  }
+
+  onClickDelete(id: number){
+    this.userService.deleteUser(id).subscribe((res) =>{
+      this.messageService.add({severity:"success", summary: "Student", detail: "Student has been deleted"});
+    })
+  }
 }
